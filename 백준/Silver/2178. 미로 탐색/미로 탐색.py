cbee2e97731209a5cb5bee28985152d486c0d226
HeeSung98 <91237087+HeeSung98@users.noreply.github.com>
2025-01-03 19:46:18 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34952 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+board = []
+visited = [[0 for _ in range(M)]for _ in range(N)]
+visited[0][0] = 1
+
+for i in range(N):
+    board.append(list(map(int, input().strip())))
+
+Q = deque([])
+Q.append([0, 0])
+
+while Q:
+    x, y = Q.popleft()
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < N and 0 <= ny < M and board[nx][ny]:
+            if not visited[nx][ny]:
+                visited[nx][ny] += visited[x][y] + 1
+                Q.append([nx, ny])
+
+print(visited[N - 1][M - 1])
